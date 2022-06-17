ff6ae34c7e22e7cf0a99d51507b03c2e9e57eff2
cotsdow0111@gmail.com <81422318+taoghetgithub71420@users.noreply.github.com>
2022-06-17 23:35:32 +0700

    s1 4

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})-> name('home');
+
+
+Route::get('/post', function () {
+    return view('post');
+})-> name('post');
 
 require __DIR__.'/auth.php';
